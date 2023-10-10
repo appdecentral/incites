24eafafced308d5a1358f793fb6a67de2ea89ad9
Drew McCormack <drewmccormack@mac.com>
2023-10-10 15:48:44 +0200

    Removed Schema(versionedSchema:...) which seems to result in a broken model. Were getting problems with inverse relationships.

--- Incites/IncitesApp.swift
+++ Incites/IncitesApp.swift
@@ -20,7 +20,7 @@ struct IncitesApp: App {
     
     var modelContainer: ModelContainer = {
         do {
-            let schema = Schema(versionedSchema: IncitesSchema2.self)
+            let schema = Schema([Incite.self], version: .init(1, 1, 0))
             let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
             return try ModelContainer(for: schema, migrationPlan: IncitesMigrationPlan.self, configurations: [config])
         } catch {
